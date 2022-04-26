a862af8b0b89cb7705db5251094a8f605298e4c6
Uday kiran <64728986+uday-kiran77@users.noreply.github.com>
2022-04-26 14:26:45 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "theme-green": "#3CCF91",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
